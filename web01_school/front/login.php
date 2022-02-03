b519561c022a157ba61d5662fb26711acdca7a68
Moon <mooncactusmoon@gmail.com>
2022-02-03 14:49:43 +0800

    完成全部

--- web01_school/front/login.php
+++ web01_school/front/login.php
@@ -1,16 +1,33 @@
+<?php
+if(isset($_SESSION['login'])){
+	to("http://127.0.0.1/level_B/web01_school/back.php");
+}
+if(!empty($_POST)){
 
-			<div class="di"
-				style="height:540px; border:#999 1px solid; width:53.2%; margin:2px 0px 0px 0px; float:left; position:relative; left:20px;">
-				<marquee scrolldelay="120" direction="left" style="position:absolute; width:100%; height:40px;">
-				<?php include "marquee.php"; ?>	
-			</marquee>
-				<div style="height:32px; display:block;"></div>
-				<!--正中央-->
-				<form method="post" action="?do=check" target="back">
-					<p class="t botli">管理員登入區</p>
-					<p class="cent">帳號 ： <input name="acc" autofocus="" type="text"></p>
-					<p class="cent">密碼 ： <input name="ps" type="password"></p>
-					<p class="cent"><input value="送出" type="submit"><input type="reset" value="清除"></p>
-				</form>
-			</div>
+	if($Admin->math('count','*',['acc'=>$_POST['acc'],'pw'=>$_POST['pw']])>0){
+		$_SESSION['login']=$_POST['acc'];
+	
+		to("http://127.0.0.1/level_B/web01_school/back.php");
+	}else{
+		echo "<script>";
+		echo "alert('帳號或密碼錯誤')";
+		echo "</script>";
+	}
+}
+?>
+<div class="di"
+	style="height:540px; border:#999 1px solid; width:53.2%; margin:2px 0px 0px 0px; float:left; position:relative; left:20px;">
+	<marquee scrolldelay="120" direction="left" style="position:absolute; width:100%; height:40px;">
+	<?php include "marquee.php"; ?>	
+</marquee>
+	<div style="height:32px; display:block;"></div>
+	<!--正中央-->
+	<!-- 修改action 刪除target -->
+	<form method="post" action="?do=login">
+		<p class="t botli">管理員登入區</p>
+		<p class="cent">帳號 ： <input name="acc" autofocus="" type="text"></p>
+		<p class="cent">密碼 ： <input name="pw" type="password"></p>
+		<p class="cent"><input value="送出" type="submit"><input type="reset" value="清除"></p>
+	</form>
+</div>
 		
--- web01_school/index.php
+++ web01_school/index.php
@@ -1,8 +1,8 @@
 <?php
 include_once "base.php";
+
 ?>
-<!DOCTYPE html
-	PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <!-- saved from url=(0040)http://127.0.0.1/test/exercise/collage/? -->
 <html xmlns="http://www.w3.org/1999/xhtml">
 
@@ -18,35 +18,34 @@ include_once "base.php";
 <body>
 	<div id="cover" style="display:none; ">
 		<div id="coverr">
-			<a style="position:absolute; right:3px; top:4px; cursor:pointer; z-index:9999;"
-				onclick="cl(&#39;#cover&#39;)">X</a>
+			<a style="position:absolute; right:3px; top:4px; cursor:pointer; z-index:9999;" onclick="cl(&#39;#cover&#39;)">X</a>
 			<div id="cvr" style="position:absolute; width:99%; height:100%; margin:auto; z-index:9898;"></div>
 		</div>
 	</div>
-	
+
 	<div id="main">
-	<?php include "front/header.php";?>
-			<!--標題-->
-		
+		<?php include "front/header.php"; ?>
+		<!--標題-->
+
 		<div id="ms">
 			<div id="lf" style="float:left;">
 				<div id="menuput" class="dbor">
 					<!--主選單放此-->
 					<span class="t botli">主選單區</span>
 					<?php
-					$mains=$Menu->all(['sh'=>1,'parent'=>0]);
+					$mains = $Menu->all(['sh' => 1, 'parent' => 0]);
 
-					foreach($mains as $main){
+					foreach ($mains as $main) {
 						//調整一下html元素和內容的關係，並且利用到題組本身提供的class
 						echo "<div class='mainmu'>";
 
 						echo "<a href='{$main['href']}'>{$main['name']}</a>";
-						if($Menu->math('count','*',['parent'=>$main['id']])>0){
-						    //在有次選單的主選單區塊中再建立一個class為mw的div區塊
+						if ($Menu->math('count', '*', ['parent' => $main['id']]) > 0) {
+							//在有次選單的主選單區塊中再建立一個class為mw的div區塊
 							echo "  <div class='mw'>";
 							//撈出該主選單的所有次選單
-							$subs=$Menu->all(['parent'=>$main['id']]);
-							foreach($subs as $sub){
+							$subs = $Menu->all(['parent' => $main['id']]);
+							foreach ($subs as $sub) {
 								echo "<div class='mainmu2'>";
 								echo "<a href='{$sub['href']}'>{$sub['name']}</a>";
 								echo "</div>";
@@ -59,57 +58,68 @@ include_once "base.php";
 				</div>
 				<div class="dbor" style="margin:3px; width:95%; height:20%; line-height:100px;">
 					<span class="t">進站總人數 :
-						<?=$Total->find(1)['total'];?> </span>
+						<?= $Total->find(1)['total']; ?> </span>
 				</div>
 			</div>
-			
+
 			<?php
-			$do=(!empty($_GET['do']))?$_GET['do']:"main";
-			$file="./front/".$do.".php";
-			if(file_exists($file)){
+			$do = (!empty($_GET['do'])) ? $_GET['do'] : "main";
+			$file = "./front/" . $do . ".php";
+			if (file_exists($file)) {
 				include $file;
-			}else{
+			} else {
 				include "./front/main.php";
 			}
 
 			?>
 
 
-			<div id="alt"
-				style="position: absolute; width: 350px; min-height: 100px; word-break:break-all; text-align:justify;  background-color: rgb(255, 255, 204); top: 50px; left: 400px; z-index: 99; display: none; padding: 5px; border: 3px double rgb(255, 153, 0); background-position: initial initial; background-repeat: initial initial;">
+			<div id="alt" style="position: absolute; width: 350px; min-height: 100px; word-break:break-all; text-align:justify;  background-color: rgb(255, 255, 204); top: 50px; left: 400px; z-index: 99; display: none; padding: 5px; border: 3px double rgb(255, 153, 0); background-position: initial initial; background-repeat: initial initial;">
 			</div>
 			<script>
 				$(".sswww").hover(
-					function () {
-						$("#alt").html("" + $(this).children(".all").html() + "").css({ "top": $(this).offset().top - 50 })
+					function() {
+						$("#alt").html("" + $(this).children(".all").html() + "").css({
+							"top": $(this).offset().top - 50
+						})
 						$("#alt").show()
 					}
 				)
 				$(".sswww").mouseout(
-					function () {
+					function() {
 						$("#alt").hide()
 					}
 				)
 			</script>
 			<div class="di di ad" style="height:540px; width:23%; padding:0px; margin-left:22px; float:left; ">
 				<!--右邊-->
-				<button style="width:100%; margin-left:auto; margin-right:auto; margin-top:2px; height:50px;"
-					onclick="lo(&#39;?do=admin&#39;)">管理登入</button>
+				<?php
+				if (isset($_SESSION['login'])) {
+				?>
+				
+				<button style="width:100%; margin-left:auto; margin-right:auto; margin-top:2px; height:50px;" onclick="lo(&#39;lo('back.php')&#39;)">返回管理</button>
+				<?php
+				}else{
+				?>
+				<button style="width:100%; margin-left:auto; margin-right:auto; margin-top:2px; height:50px;" onclick="lo(&#39;?do=login&#39;)">管理登入</button>
+				<?php
+				}
+				?>
 				<div style="width:89%; height:480px;" class="dbor">
 					<span class="t botli">校園映象區</span>
 					<!-- 建立div放置up.jpg,並建立onclick事件啟用pp(1)函式 -->
 					<div class="t" onclick="pp(1)"><img src="icon/up.jpg"></div>
 					<?php
 					//撈出所有顯式的校園映像
-					$imgs=$Image->all(['sh'=>1]);
-					foreach($imgs as $key =>$img){
+					$imgs = $Image->all(['sh' => 1]);
+					foreach ($imgs as $key => $img) {
 					?>
-					<!-- 內建js會用‵.im‵來使所有圖片先隱藏。`ssaa`+字串作為id，所以用迴圈的\$key來建置每張圖片不同的id -->
-					<div class="im cent" id="ssaa<?=$key;?>">
-						<!-- 題目要求圖片要有150px*103px -->
-						<img src="img/<?=$img['img'];?>" style="width:150px;height:103px;margin:1px">
-					</div>
-					<?php	
+						<!-- 內建js會用‵.im‵來使所有圖片先隱藏。`ssaa`+字串作為id，所以用迴圈的\$key來建置每張圖片不同的id -->
+						<div class="im cent" id="ssaa<?= $key; ?>">
+							<!-- 題目要求圖片要有150px*103px -->
+							<img src="img/<?= $img['img']; ?>" style="width:150px;height:103px;margin:1px">
+						</div>
+					<?php
 					}
 					?>
 					<!-- 建立一個div放置dn.jpg，並建立onclick啟用pp(2) -->
@@ -117,14 +127,19 @@ include_once "base.php";
 					<script>
 						var nowpage = 0;
 						//num用來記錄有多少張圖片要顯示，所以要用php計算圖片數量
-						var num = <?=$Image->math("count",'*',['sh'=>1]);?>;
+						var num = <?= $Image->math("count", '*', ['sh' => 1]); ?>;
+
 						function pp(x) {
 							var s, t;
-							if (x == 1 && nowpage - 1 >= 0) { nowpage--; }
-							
+							if (x == 1 && nowpage - 1 >= 0) {
+								nowpage--;
+							}
+
 							//原本的js向下輪播有錯，要修改
-							if (x == 2 && (nowpage + 3) <= num) { nowpage++; }
-							
+							if (x == 2 && (nowpage + 3) <= num) {
+								nowpage++;
+							}
+
 							$(".im").hide()
 							for (s = 0; s <= 2; s++) {
 								t = s * 1 + nowpage * 1;
@@ -137,9 +152,8 @@ include_once "base.php";
 			</div>
 		</div>
 		<div style="clear:both;"></div>
-		<div
-			style="width:1024px; left:0px; position:relative; background:#FC3; margin-top:4px; height:123px; display:block;">
-			<span class="t" style="line-height:123px;"><?=$Bottom->find(1)['bottom'];?></span>
+		<div style="width:1024px; left:0px; position:relative; background:#FC3; margin-top:4px; height:123px; display:block;">
+			<span class="t" style="line-height:123px;"><?= $Bottom->find(1)['bottom']; ?></span>
 		</div>
 	</div>
 
