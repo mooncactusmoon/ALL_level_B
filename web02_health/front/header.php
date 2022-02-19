a941472ff666a4a90f6d5593641bf23087aa4808
Moon <mooncactusmoon@gmail.com>
2022-02-19 16:42:33 +0800

    完成初步整理版面

---
+++ web02_health/front/header.php
@@ -0,0 +1,7 @@
+<div id="title">
+        <?=date("m月d號 l");?> | 今日瀏覽: 1 | 累積瀏覽: 36        </div>
+        <div id="title2">
+            <a href="index.php" title="健康促進網 - 回首頁">
+                <img src="./icon/02B01.jpg" >
+            </a>
+        </div>
